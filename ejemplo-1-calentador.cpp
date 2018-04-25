ea5635bf97a9f02d6142389717d39a85c3295c72
luis97lopez <lopezromeroluisalberto472@gmail.com>
2018-04-25 15:37:40 +0000

    Se crea la clase calentador con métodos para calentar y enfríar

---
+++ ejemplo-1-calentador.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+class Calentador
+{
+    private:
+        int temperatura;
+    
+    public:
+        Calentador();
+        void calentar();
+        void enfriar();
+        void imprimeTemperatura();
+};  
+
+Calentador::Calentador()
+{
+    temperatura = 15;
+}
+
+void Calentador :: calentar()
+{
+    temperatura += 5;
+}
+
+void Calentador :: enfriar()
+{
+    temperatura -= 5;
+}
+
+void Calentador :: imprimeTemperatura()
+{
+    std :: cout  << "La temperatura es " << temperatura << std :: endl;
+}
+
+int main()
+{
+    Calentador c1,c2;
+    c1.calentar();
+    c1.imprimeTemperatura();
+    
+    c2.enfriar();
+    c2.imprimeTemperatura();
+    return 0;
+}
