7b81a0e7d820fb4d2c4324579e6a66805c9a9d0e
luis97lopez <lopezromeroluisalberto472@gmail.com>
2018-05-07 16:02:20 +0000

    Se implementó la captura e impresión con sobrecarga de operandos

--- ejercicio.cpp
+++ ejercicio.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-
 class Fraccion
 {
+    friend void operator<<(std :: ostream &, Fraccion); //Permite acceder a las variables privadas
+    friend void operator>>(std :: istream &, Fraccion &);
     private:
         int numerador;
         int denominador;
@@ -19,11 +20,6 @@ Fraccion :: Fraccion(int numerador, int denominador)
 {
     this->numerador = numerador;
     this->denominador = denominador;
-}
-
-void Fraccion :: imprimeFraccion()
-{
-    std :: cout << numerador << " / " << denominador << std::endl;
 }
 
 Fraccion Fraccion :: operator+(Fraccion otro)
@@ -54,15 +50,32 @@ Fraccion Fraccion :: operator/(Fraccion otro)
     return f;
 }
 
+void operator << (std :: ostream &salida, Fraccion f)
+{
+    salida << f.numerador << " / " << f.denominador << std :: endl;
+    
+}
+
+void operator >> (std :: istream &entrada, Fraccion &f)
+{
+    entrada >> f.numerador;
+    entrada >> f.denominador;
+}
+
 int main()
 {
     Fraccion f1{3,8};
     Fraccion f2{4,5};
-    f1.imprimeFraccion();
-    f2.imprimeFraccion();
-    f1.operator+(f2).imprimeFraccion();
-    f1.operator-(f2).imprimeFraccion();
-    f1.operator*(f2).imprimeFraccion();
-    f1.operator/(f2).imprimeFraccion();
+    
+    std :: cin >> f1;
+    std :: cin >> f2;
+    
+    std :: cout << f1;
+    std :: cout << f2;
+    
+    std :: cout << (f1 + f2);
+    std :: cout << (f1 - f2);
+    std :: cout << (f1 * f2);
+    std :: cout << (f1 / f2);
     return 0;
 }
